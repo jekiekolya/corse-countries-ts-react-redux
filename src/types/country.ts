cedb6e5fc377527c251968518057e97850a757de
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-13 22:54:26 +0200

    add main types

---
+++ src/types/country.ts
@@ -0,0 +1,29 @@
+import { Region } from "./regions";
+
+export type Currency = {
+  code: string;
+  name: string;
+  symbol: string;
+};
+
+export type Language = {
+  iso639_1: string;
+  iso639_2: string;
+  name: string;
+  nativeName: string;
+};
+
+export type Country = {
+  name: string;
+  nativeName: string;
+  flag: string;
+  flags: [{ png: string }, { svg: string }];
+  region: Region;
+  subregion: string;
+  capital: string;
+  population: number;
+  topLevelDomain: string[];
+  borders: string[];
+  currency: Currency[];
+  languages: Language[];
+};
