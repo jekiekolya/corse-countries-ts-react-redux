44a1d5bac2db650a43597fd978614e753b7c2044
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-13 20:19:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import {Provider} from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-import {store} from './store';
-
+import { store } from "./store";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
@@ -15,7 +14,7 @@ const root = ReactDOMClient.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="corse-countries-ts-react-redux/">
         <App />
       </BrowserRouter>
     </Provider>
