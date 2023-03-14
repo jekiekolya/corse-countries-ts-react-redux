924dfb4637b4ae89c36a1168e2c80df810d0cdd4
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-14 13:27:54 +0200

    add types to counries

--- src/features/countries/CountryList.tsx
+++ src/features/countries/CountryList.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
-import { List } from "../../components/List";
-import { Card } from "../../components/Card";
+import { List } from "components/List";
+import { Card } from "components/Card";
 import { useCountries } from "./use-countries";
 // import { CountryInfo } from "types";
 
